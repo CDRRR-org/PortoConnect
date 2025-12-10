b7313f69fb6078570d2555477b9fd853ea4a8983
Lucinus <radityapamumgkas@gmail.com>
2025-12-10 15:07:36 +0700

    cleaning

--- Frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/main.css' // jika ada, kalau tidak ada ok juga
-
-createApp(App).use(router).mount('#app')
